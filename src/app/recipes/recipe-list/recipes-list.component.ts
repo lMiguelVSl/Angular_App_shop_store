6ca5ce9abc4eb170f82d0a5987ee1cca583c8ffd
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-03-26 12:01:51 -0500

    recipe list component view done

--- src/app/recipes/recipe-list/recipes-list.component.ts
+++ src/app/recipes/recipe-list/recipes-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes-models/recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent {
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Recipe Test', 'It is a test', 'URL test')
+  ];
 
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
